981c03ddffcf9beef198f3123b654225f6d26da0
Lea Bugeat <lbugeat@gmail.com>
2020-08-25 15:03:45 +0200

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Trip.destroy_all
+User.destroy_all
+puts "destroy all"
+
+lea = User.create(email: "lea@gmail.com", first_name: "Lea", last_name: "Wagon", address: "Merignac", phone_number: "0123456789", password: "azerty")
+lea.save!
+adrien = User.create(email: "adrien@gmail.com", first_name: "Adrien", last_name: "Wagon", address: "Paris", phone_number: "0123656089", password: "azerty")
+adrien.save!
+anais = User.create(email: "anais@gmail.com", first_name: "Anais", last_name: "Wagon", address: "Orléans", phone_number: "0193454782", password: "azerty")
+anais.save!
+ludo = User.create(email: "ludo@gmail.com", first_name: "Ludo", last_name: "Wagon", address: "Bordeaux", phone_number: "0113856784", password: "azerty")
+ludo.save!
+
+puts "created"
