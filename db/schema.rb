825cb897a31e64573648fc6614e3375e8d00cc38
Adrien Loiseau <adrien.lois@gmail.com>
2020-08-26 12:36:00 +0200

    Creating API visio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_134230) do
+ActiveRecord::Schema.define(version: 2020_08_26_090635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_134230) do
     t.bigint "repertoire_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "video_link"
     t.index ["repertoire_id"], name: "index_contacts_on_repertoire_id"
     t.index ["user_id"], name: "index_contacts_on_user_id"
   end
