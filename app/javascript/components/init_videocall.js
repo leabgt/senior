edbbf4a6f381151f40cce468b7d8892cf4bb7309
Adrien Loiseau <adrien.lois@gmail.com>
2020-08-27 12:32:53 +0200

    fixed prod problem

--- app/javascript/components/init_videocall.js
+++ app/javascript/components/init_videocall.js
@@ -16,4 +16,27 @@ const initVideoCall = () => {
   xhr.send(data);
 }
 
+// export { initVideoCall }
+
+
+// const data = JSON.stringify({
+//   "properties": {
+//     "meeting_join_hook": "\"<%= @contact.video_link %>\""
+//   }
+// });
+
+// const xhr = new XMLHttpRequest();
+
+// xhr.addEventListener("readystatechange", function () {
+//   if (this.readyState === this.DONE) {
+//     console.log(this.responseText);
+//   }
+// });
+
+// xhr.open("POST", "https://api.daily.co/v1/");
+// xhr.setRequestHeader("content-type", "application/json");
+// xhr.setRequestHeader("authorization", "Bearer 966025264c5a9e63e67b677f531cfa4ef52a9bb32e1389d92773999c70e90241");
+
+// xhr.send(data);
+
 export { initVideoCall }
