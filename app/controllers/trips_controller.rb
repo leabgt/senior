66c217ccc9853a54c9d41a1e255b76245eebd8c1
Lea Bugeat <lbugeat@gmail.com>
2020-08-27 16:29:43 +0200

    update

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -7,11 +7,23 @@ class TripsController < ApplicationController
   end
 
   def create
+    @trip = Trip.new(trip_params)
+    if @trip.save!
+      redirect_to edit_trip(@trip)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:arrival_address, :start_at, :timeslot)
   end
 end
