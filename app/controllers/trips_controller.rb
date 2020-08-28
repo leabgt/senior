43e01c02f16989bc764b614211244c96aeeb4567
Lea Bugeat <lbugeat@gmail.com>
2020-08-28 14:23:32 +0200

    insert Uber

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,5 @@
 class TripsController < ApplicationController
+
   def show
     @trip = Trip.find(params[:id])
   end
@@ -10,9 +11,8 @@ class TripsController < ApplicationController
   def create
     @trip = Trip.new(trip_params)
     @trip.user = current_user
-    if @trip.save!
-      redirect_to edit_trip_path(@trip)
-      # "https://m.uber.com/ul/?client_id=<CLIENT_ID>&action=setPickup&pickup[latitude]=37.775818&pickup[longitude]=-122.418028&pickup[nickname]=UberHQ&pickup[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103&dropoff[latitude]=37.802374&dropoff[longitude]=-122.405818&dropoff[nickname]=Coit%20Tower&dropoff[formatted_address]=1%20Telegraph%20Hill%20Blvd%2C%20San%20Francisco%2C%20CA%2094133&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d"
+    if @trip.save
+      redirect_to "https://m.uber.com/?client_id=#{@trip.user.id}&action=setPickup&pickup[latitude]=#{@trip.user.latitude}&pickup[longitude]=#{@trip.user.longitude}&pickup[nickname]=#{@trip.user.address}&pickup[formatted_address]=#{@trip.user.address}&dropoff[latitude]=#{@trip.latitude}&dropoff[longitude]=#{@trip.longitude}&dropoff[nickname]=#{@trip.arrival_address}&dropoff[formatted_address]=#{@trip.arrival_address}&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d"
     else
       render :new
     end
@@ -29,6 +29,7 @@ class TripsController < ApplicationController
   private
 
   def trip_params
-    params.require(:trip).permit(:arrival_address, :start_at, :timeslot)
+    params.require(:trip).permit(:arrival_address, :start_at, :timeslot, :longitude, :latitude)
   end
+
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :user
+
   geocoded_by :arrival_address
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_arrival_address?
 end
