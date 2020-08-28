15edd0dfccf50741fe199e369b8e89481cade48a
Lea Bugeat <lbugeat@gmail.com>
2020-08-28 11:40:31 +0200

    lalala

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -12,6 +12,7 @@ class TripsController < ApplicationController
     @trip.user = current_user
     if @trip.save!
       redirect_to edit_trip_path(@trip)
+      # "https://m.uber.com/ul/?client_id=<CLIENT_ID>&action=setPickup&pickup[latitude]=37.775818&pickup[longitude]=-122.418028&pickup[nickname]=UberHQ&pickup[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103&dropoff[latitude]=37.802374&dropoff[longitude]=-122.405818&dropoff[nickname]=Coit%20Tower&dropoff[formatted_address]=1%20Telegraph%20Hill%20Blvd%2C%20San%20Francisco%2C%20CA%2094133&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d"
     else
       render :new
     end
