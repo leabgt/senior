9f57e8b42f90c7968411be1d32a7101d3ffd7424
Adrien Loiseau <adrien.lois@gmail.com>
2020-09-01 15:22:09 +0200

    seeds description

--- db/seeds.rb
+++ db/seeds.rb
@@ -67,44 +67,42 @@ contact_Ludo3 = Contact.create(repertoire: repertoire_Ludo, user: anais, video_l
 puts "creating meals"
 
 file = URI.open("https://res.cloudinary.com/dyrbktlhi/image/upload/v1598622787/breakfast.jpg")
-breakfast = Meal.new(name: "salty", category: "breakfast", description: "These are the ingredients you’ll need for this healthy breakfast sandwich: well-seeded organic bread or artisan bread, Ricotta cheese, sea salt flakes, preserves – wild blueberry or your favorite flavor, Macadamia nuts, and organic honey." )
+breakfast = Meal.new(name: "Salty", category: "breakfast", description: "These are the ingredients you’ll need for this healthy breakfast sandwich." )
 breakfast.photo.attach(io: file, filename: "breakfast.jpg", content_type: "image/jpg")
 breakfast.save!
 
 file = URI.open("https://res.cloudinary.com/dyrbktlhi/image/upload/v1598616625/pancakes.jpg")
-pancakes = Meal.new(name: "pancakes", category: "breakfast", description: "No sweet pancakes here! (Save those for Sunday morning brunch.) These pancakes are great for weekday mornings when you need some fuel in the morning to get through the day without spiking blood sugar levels. This savory pancake recipe has onions and meat to fill you up and provide some good nutrition. And while it takes 45 minutes to make, you can do meal prep and make a large batch at once to save for weekday mornings by simply re-heating in minutes." )
+pancakes = Meal.new(name: "pancakes", category: "breakfast", description: "No sweet pancakes here! These pancakes are great for weekday mornings when you need some fuel in the morning to get through the day without spiking blood sugar levels." )
 pancakes.photo.attach(io: file, filename: "pancakes.jpg", content_type: "image/jpg")
 pancakes.save!
 
 file = URI.open("https://res.cloudinary.com/dyrbktlhi/image/upload/v1598616837/salmon.jpg")
-salmon = Meal.new(name: "salmon", category: "lunch", description: "Heat control when cooking fish is notoriously difficult. Use the steam mode for perfectly tender and juicy results! Salmon is high in protein and low in fat, and with the addition of vitamin B-rich asparagus, is a great support to those building muscle." )
+salmon = Meal.new(name: "Salmon", category: "lunch", description: "Heat control when cooking fish is notoriously difficult. Use the steam mode for perfectly tender and juicy results!" )
 salmon.photo.attach(io: file, filename: "salmon.jpg", content_type: "image/jpg")
 salmon.save!
 
 file = URI.open("https://res.cloudinary.com/dyrbktlhi/image/upload/v1598618045/beef.jpg")
-beef = Meal.new(name: "beef", category: "lunch" , description: "Le filet est une tranche fine de viande, prélevée sur la poitrine, dans le sens de la longueur sur une carcasse bouchère ou un poisson. C'est une partie tendre et maigre.
-Le filet peut être tranché pour être simplement grillé ou poêlé en cuisine, ou pour être accommodé dans les recettes les plus complexes de la cuisine gastronomique.
-Dans le cas de la commercialisation des découpes d'une carcasse bouchère vouée à l'alimentation, les filets font partie des pièces de viande au tarif élevé." )
+beef = Meal.new(name: "Beef", category: "lunch" , description: "Succulent roast topside of beef, made especially irresistible with a mustard crust, piquant shallots and red wine enriched gravy." )
 beef.photo.attach(io: file, filename: "beef.jpg", content_type: "image/jpg")
 beef.save!
 
 file = URI.open("https://res.cloudinary.com/dyrbktlhi/image/upload/v1598618316/salad.jpg")
-salad = Meal.new(name: "salad", category: "lunch", description: "A Caesar salad (also spelled Cesar and Cesare) is a green salad of romaine lettuce and croutons dressed with lemon juice (or lime juice), olive oil, egg, Worcestershire sauce, anchovies, garlic, Dijon mustard, Parmesan cheese, and black pepper." )
+salad = Meal.new(name: "Salad", category: "lunch", description: "Give salad a shake-up with an Egyptian-inspired recipe that combines fava beans with egg and the rich flavours of tahini, garlic, lemon and cumin." )
 salad.photo.attach(io: file, filename: "salad.jpg", content_type: "image/jpg")
 salad.save!
 
 file = URI.open("https://res.cloudinary.com/dyrbktlhi/image/upload/v1598618426/veal.jpg")
-veal = Meal.new(name: "veal", category: "diner", description: "You’ll often see veal shanks cut crosswise into osso buco, but around the holidays, we like to braise them whole for a dramatic presentation. Call in advance so the butcher sets aside the entire pieces for you. There’s no need to have them trimmed; the meat will shrink on the bones as it braises." )
+veal = Meal.new(name: "Veal", category: "diner", description: "Marinate rose veal chops in a zesty mix of lemon, garlic and rosemary then serve with the salsa for a simple yet flavourful supper." )
 veal.photo.attach(io: file, filename: "veal.jpg", content_type: "image/jpg")
 veal.save!
 
 file = URI.open("https://res.cloudinary.com/dyrbktlhi/image/upload/v1598621760/pestopasta.jpg")
-pestopasta = Meal.new(name: "pesto pasta", category: "diner", description: "pesto pasta; Pesto, or to refer to the original dish pesto alla genovese, is a sauce originating in Genoa, the capital city of Liguria, Italy.[1] It traditionally consists of crushed garlic, European pine nuts, coarse salt, basil leaves, and hard cheese such as Parmigiano-Reggiano (also known as Parmesan cheese) or Pecorino Sardo (cheese made from sheep's milk), all blended with olive oil.[2]" )
+pestopasta = Meal.new(name: "Pesto Pasta", category: "diner", description: "A fresh and flavour-packed homemade pesto with peas and pine nuts" )
 pestopasta.photo.attach(io: file, filename: "pestopasta.jpg", content_type: "image/jpg")
 pestopasta.save!
 
 file = URI.open("https://res.cloudinary.com/dyrbktlhi/image/upload/v1598618695/fish.jpg")
-fish = Meal.new(name: "fish", category: "diner", description: "Cod vegetables: Cod is the common name for the demersal fish genus Gadus, belonging to the family Gadidae. ... Cod is popular as a food with a mild flavour and a dense, flaky, white flesh" )
+fish = Meal.new(name: "Fish", category: "diner", description: "Cod vegetables: Cod is the common name for the demersal fish genus Gadus, belonging to the family Gadidae." )
 fish.photo.attach(io: file, filename: "fish.jpg", content_type: "image/jpg")
 fish.save!
 
