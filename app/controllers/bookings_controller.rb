9d03358d028d636af36c456d6df1b5ec51cf86c4
Adrien Loiseau <adrien.lois@gmail.com>
2020-09-01 15:57:59 +0200

    evolving

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
 
-   def show
+  def show
     @booking = Booking.find(params[:id])
     @meal = Meal.find(params[:meal_id])
   end
@@ -9,4 +9,16 @@ class BookingsController < ApplicationController
     @meal = Meal.find(params[:meal_id])
     @booking = Booking.new
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @meal = Meal.find(params[:meal_id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :meal_id)
+  end
+
 end
