16787588f8164a59b5356555928778ea85880f68
Lea Bugeat <lbugeat@gmail.com>
2020-09-01 11:06:25 +0200

    try

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  # app/controllers/application_controller.rb
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
